b659d084cef0a6dd27e650a0d046f9694e3f3b95
zhou <killian.zhou@sepefrei.fr>
2021-06-03 23:59:23 +0200

    nav bar circle  ?

---
+++ files/js/navBar.js
@@ -0,0 +1,7 @@
+// let links = document.getElementById("nav-bar").children;
+// let shift = -10, range=5;
+// for (let i = 0; i < links.length; i++){
+//     links[i].style.transform = "rotate(" + shift.toString() + "deg);";
+//     console.log(links[i].style.transform, "rotate(" + shift + "deg)");
+//     shift += range;
+// }
